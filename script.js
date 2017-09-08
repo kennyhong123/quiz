38e1aeefe14857ede18ed9c4281e7dde99c4a306
kennyhong123 <31251440+kennyhong123@users.noreply.github.com>
2017-09-08 09:03:07 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
     $("#button1").click(function() {
         $("#memes1").toggle();
-    })
+    });
 	$("#button2").click(function() {
         $("#memes2").toggle();
-    })
+    });
 	$("#button3").click(function() {
         $("#memes3").toggle();
-    })
+    });
 	$("#button4").click(function() {
         $("#memes4").toggle();
-    })
+    });
 	$("#button5").click(function() {
         $("#memes5").toggle();
-    })
+    });
 });
