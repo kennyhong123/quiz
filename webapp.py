216bbd2a130a249abbf9232e25123ce889ddefcf
programming-john <johnathoncymoore@gmail.com>
2018-02-02 09:34:58 -0800

    fixed

--- webapp.py
+++ webapp.py
@@ -11,23 +11,15 @@ app.secret_key=os.environ["SECRET_KEY"];
 @app.route('/')
 def renderMain():
     return render_template('index.html')
-  
+	
+@app.route('/retake')
+def renderBack():
+	session.clear()
+	return render_template('index.html')
+	
 @app.route('/response',methods=['GET','POST'])
 def renderScore():
-    session['score'] = 0
-    session['q1'] = request.form['a1']
-    session['q2'] = request.form['a2']
-    session['q3'] = request.form['a3']
-    session['q4'] = request.form['a4']
-     if request.form['n1'] == "yes":
-      session['score'] += 1
-    if request.form['n2'] == "":
-      session['score'] += 1
-    if request.form['n3'] == "tuna":
-      session['score'] += 1
-    if request.form['n4'] == "great white":
-      session['score'] += 1
-    return render_template('.html')
+	return render_template('score.html')
   
     
 if __name__=="__main__":
